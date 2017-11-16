4c133c5ff7c68c97a1945f0074073152d19a01d0
Stevan <steeeveize@gmail.com>
2017-11-16 09:24:15 +0100

    Servis

--- ProjectSBES/Service/Program.cs
+++ ProjectSBES/Service/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,15 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            ServiceHost _serviceHost = new ServiceHost(typeof(ServiceApp));
+            _serviceHost.Open();
+
+            Console.WriteLine("SERVER: Ready and waiting for requests. Press any key to exit.");
+
+            Console.ReadLine();
+
+            _serviceHost.Close();
+            Console.WriteLine("SERVER: Stopped.");
         }
     }
 }
