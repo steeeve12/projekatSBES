7a422467ce949dbf7f42ca2e91feee63179f23a6
steeeve12 <steeeveize@gmail.com>
2017-11-17 17:18:32 +0100

    CustomAuthorization Manager i Policy

---
+++ ProjectSBES/Contracts/CustomAuthorizationManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class CustomAuthorizationManager : ServiceAuthorizationManager
+    {
+        protected override bool CheckAccessCore(OperationContext operationContext)
+        {
+            //bool authorized = false;
+            IPrincipal principal = operationContext.ServiceSecurityContext.AuthorizationContext.Properties["Principal"] as IPrincipal;
+
+            if (principal != null)
+            {
+                //    authorized = (principal as CustomPrincipal).IsInRole(Permissions.View.ToString());
+
+                //    if (authorized == false)
+                //    {
+                //        /// audit authorization failed event	
+                //        Audit.AuthorizationFailed(principal.Identity.Name, OperationContext.Current.IncomingMessageHeaders.Action, "Authorization failed. You don't have permission for this operation.");
+                //    }
+                //    else
+                //    {
+                //        /// audit successfull authorization event
+                //        /// Audit.AuthorizationSuccess(principal.Identity.Name, OperationContext.Current.IncomingMessageHeaders.Action);
+                //    }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
