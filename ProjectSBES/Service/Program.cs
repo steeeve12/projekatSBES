4c0ed05ec2f4001ad27adfa8afb14233e84eb279
Branko <branici@gmail.com>
2017-11-17 19:20:35 +0100

    ServiceApp zabrana korisnika, AppConfig na Client i dodavanje singlton klase za upravljanje podacima na ServiceApp

--- ProjectSBES/Service/Program.cs
+++ ProjectSBES/Service/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Contracts;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IdentityModel.Policy;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +16,17 @@ namespace Service
         static void Main(string[] args)
         {
             ServiceHost _serviceHost = new ServiceHost(typeof(ServiceApp));
+            _serviceHost.Authorization.ServiceAuthorizationManager = new CustomAuthorizationManager();
+
+            List<IAuthorizationPolicy> policies = new List<IAuthorizationPolicy>();
+            policies.Add(new CustomAuthorizationPolicy());
+            _serviceHost.Authorization.ExternalAuthorizationPolicies = policies.AsReadOnly();
+
+            _serviceHost.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
+
+            _serviceHost.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            _serviceHost.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+
             _serviceHost.Open();
 
             Console.WriteLine("SERVER: Ready and waiting for requests. Press any key to exit.");
---
+++ ProjectSBES/Service/ServiceDataHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ServiceDataHelper
+    {
+        public Dictionary<string, int> usersAttempts;
+        public Dictionary<string, Stopwatch> forbidenUsers;
+        public int maxAttempts = 2;
+        public int timeOfDenial = 1000 * 60 * 1;
+
+        private static ServiceDataHelper serviceDataHelper = null;
+
+        private ServiceDataHelper()
+        {
+            usersAttempts = new Dictionary<string, int>();
+            forbidenUsers = new Dictionary<string, Stopwatch>();
+        }
+
+        public static ServiceDataHelper Helper()
+        {
+            if(serviceDataHelper == null)
+            {
+                serviceDataHelper = new ServiceDataHelper();
+            }
+            return serviceDataHelper;
+        }
+    }
+}
