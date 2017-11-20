b3a3573153e1c3209e58776ca92ce93d5034bdbe
Branko <branici@gmail.com>
2017-11-20 11:22:00 +0100

    ServiceProcy klasa implementacija.

---
+++ ProjectSBES/Service/ServiceProxy.cs
@@ -0,0 +1,37 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ServiceProxy : ChannelFactory<IAuditService>, IAuditService
+    {
+
+        private static ServiceProxy audit = null;
+        private IAuditService factory;
+
+        public ServiceProxy(string endpointConfigurationName) : base(endpointConfigurationName)
+        {
+            factory = this.CreateChannel();
+        }
+
+        public static ServiceProxy Audit(string endpointConfigurationName)
+        {
+            if(audit == null)
+            {
+                return new ServiceProxy(endpointConfigurationName);
+            }
+
+            return audit;
+        }
+
+        public bool WriteEvent(SecurityEvent sEvent)
+        {
+            return audit.factory.WriteEvent(sEvent);
+        }
+    }
+}
