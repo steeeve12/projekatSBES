f22d4c2863a2684887afb876ee98d6eed2badf68
steeeve12 <steva.borus@gmail.com>
2017-11-21 12:03:41 +0100

    Klijent bira server koji ce da gadja

--- ProjectSBES/Client/Program.cs
+++ ProjectSBES/Client/Program.cs
@@ -12,12 +12,35 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter endpoint name of server you want to target [Contracts.IService.ServerOne, Contracts.IService.ServerTwo, Contracts.IService.ServeThree]: ");
-            string endpointConfigurationName = Console.ReadLine();
+            string endpointConfigurationName = "";
+            int serverNum;
+            bool ok;
+
+            do
+            {
+                Console.WriteLine("Enter endpoint name of server you want to target [1, 2 or 3]: ");
+                ok = Int32.TryParse(Console.ReadLine(), out serverNum);
+
+                switch (serverNum)
+                {
+                    case 1:
+                        endpointConfigurationName = "Contracts.IService.ServerOne";
+                        break;
+                    case 2:
+                        endpointConfigurationName = "Contracts.IService.ServerTwo";
+                        break;
+                    case 3:
+                        endpointConfigurationName = "Contracts.IService.ServerThree";
+                        break;
+                    default:
+                        ok = false;
+                        break;
+                }
+            } while (!ok);
 
             using (ClientProxy proxy = new ClientProxy(endpointConfigurationName))
             {
-                EProcessType procces = EProcessType.GoogleChrome;
+                EProcessType procces = EProcessType.VisualStudio15;
 
                 if(proxy.RunProcess(procces))
                 {
@@ -29,6 +52,6 @@ namespace Client
                 }         
             }
             Console.ReadLine();
-        }
+        } 
     }
 }
