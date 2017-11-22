0914013112ccc6ab0a5e931ce89adf86f3ef6c4d
steeeve12 <steva.borus@gmail.com>
2017-11-22 11:39:21 +0100

    Klijent namesten i istestiran

--- ProjectSBES/Client/Program.cs
+++ ProjectSBES/Client/Program.cs
@@ -14,47 +14,80 @@ namespace Client
         {
             string endpointConfigurationName = "";
             int serverNum;
+            int processNum;
+            EProcessType processToExecute = EProcessType.GoogleChrome;
             bool ok;
+            bool end = false;
 
-            do
-            {
-                Console.WriteLine("Enter endpoint name of server you want to target [1, 2 or 3]: ");
-                ok = Int32.TryParse(Console.ReadLine(), out serverNum);
+            do {
 
-                switch (serverNum)
+                do
                 {
-                    case 1:
-                        endpointConfigurationName = "Contracts.IService.ServerOne";
-                        break;
-                    case 2:
-                        endpointConfigurationName = "Contracts.IService.ServerTwo";
-                        break;
-                    case 3:
-                        endpointConfigurationName = "Contracts.IService.ServerThree";
-                        break;
-                    default:
-                        ok = false;
-                        break;
-                }
-            } while (!ok);
+                    Console.WriteLine("Enter endpoint name of server you want to target [1, 2 or 3]: ");
+                    ok = Int32.TryParse(Console.ReadLine(), out serverNum);
 
-            using (ClientProxy proxy = new ClientProxy(endpointConfigurationName))
-            {
-                for (int i = 0; i < 1; i++)
-                {
-                    EProcessType procces = EProcessType.GoogleChrome;
-
-                    if (proxy.RunProcess(procces))
+                    switch (serverNum)
                     {
-                        Console.WriteLine(procces.ToString() + " is started!");
+                        case 1:
+                            endpointConfigurationName = "Contracts.IService.ServerOne";
+                            break;
+                        case 2:
+                            endpointConfigurationName = "Contracts.IService.ServerTwo";
+                            break;
+                        case 3:
+                            endpointConfigurationName = "Contracts.IService.ServerThree";
+                            break;
+                        default:
+                            ok = false;
+                            break;
+                    }
+                } while (!ok);
+
+                do
+                {
+                    Console.WriteLine("Enter process you wish to execute [1-GoogleChrome, 2-Mozilla, 3-Notepad, 4-VisualStudio15]: ");
+                    ok = Int32.TryParse(Console.ReadLine(), out processNum);
+
+                    switch (processNum)
+                    {
+                        case 1:
+                            processToExecute = EProcessType.GoogleChrome;
+                            break;
+                        case 2:
+                            processToExecute = EProcessType.Mozilla;
+                            break;
+                        case 3:
+                            processToExecute = EProcessType.Notepad;
+                            break;
+                        case 4:
+                            processToExecute = EProcessType.VisualStudio15;
+                            break;
+                        default:
+                            ok = false;
+                            break;
+                    }
+                } while (!ok);
+
+                using (ClientProxy proxy = new ClientProxy(endpointConfigurationName))
+                {
+                    if (proxy.RunProcess(processToExecute))
+                    {
+                        Console.WriteLine(processToExecute.ToString() + " is started!");
                     }
                     else
                     {
-                        Console.WriteLine(procces.ToString() + " is not started!");
+                        Console.WriteLine(processToExecute.ToString() + " is not started!");
                     }
+
                 }
-            }
-            Console.ReadLine();
-        } 
+
+                Console.WriteLine("Enter x if you want to exit or any other key to continue..");
+                string read = Console.ReadLine();
+
+                if (read.Equals("x"))
+                    end = true;
+
+            } while (!end);
+        }
     }
 }
