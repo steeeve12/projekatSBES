82df7ce17b2efb5e434636ae9f2069080ca93309
Doroteja <Administrator@P04-06>
2017-11-23 11:31:26 +0100

    Testovi-izmjena

--- ProjectSBES/Client/Program.cs
+++ ProjectSBES/Client/Program.cs
@@ -15,7 +15,6 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Debugger.Launch();
             bool ok = false;
             bool end = false;
             int scenario;
@@ -39,7 +38,7 @@ namespace Client
                             else
                             {
                                 Console.BackgroundColor = ConsoleColor.DarkRed;
-                                Console.WriteLine("Test Case 1 failed because user is on a BlackList for every process.");
+                                Console.WriteLine("Test Case 1 failed.");
                                 Console.BackgroundColor = ConsoleColor.Black;
                             }
                             break;
@@ -53,7 +52,7 @@ namespace Client
                             else
                             {
                                 Console.BackgroundColor = ConsoleColor.DarkRed;
-                                Console.WriteLine("Test Case 2 failed because user does not have forbidden processes.");
+                                Console.WriteLine("Test Case 2 failed.");
                                 Console.BackgroundColor = ConsoleColor.Black;
                             }
                             break;
@@ -210,8 +209,7 @@ namespace Client
 
                     if (onBlackList == usersFromBlackListPerProcess.Contains(user))
                     {
-                        Run(endpointConfigurationName, processToExecute);
-                        retVal = true;
+                        retVal =  Run(endpointConfigurationName, processToExecute);                        
                         break;
                     }
                 }
@@ -225,8 +223,9 @@ namespace Client
         /// </summary>
         /// <param name="endpointConfigurationName"></param>
         /// <param name="processToExecute"></param>
-        static void Run(string endpointConfigurationName, EProcessType processToExecute)
+        static bool Run(string endpointConfigurationName, EProcessType processToExecute)
         {
+            bool retVal = false;
 
             try
             {
@@ -238,6 +237,7 @@ namespace Client
                         if (proxy.RunProcess(processToExecute))
                         {
                             Console.WriteLine(processToExecute.ToString() + " is started!");
+                            retVal = true;
                         }
                         else
                         {
@@ -255,6 +255,8 @@ namespace Client
             catch (Exception e)
             {
             }
+
+            return retVal;
         }
     }
 }
